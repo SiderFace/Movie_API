d7e1938d186e4749cc30c19f68c74ae83e550415
SiderFace <115373215+SiderFace@users.noreply.github.com>
2023-03-11 05:06:32 -0500

    Movie API
    Setting up the directories and basic filler code and text to prepare for the building of the movie API.

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 const url = require('url');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
+   let addr = request.url;
+   let q = url.parse(addr, true);
+   let filePath = '';
+  
+
+fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+   if (err) {
+     console.log(err);
+   } else {
+     console.log('Added to log.');
+   }
+});
+
+if (q.pathname.includes('documentation')) {
+   filePath = (__dirname + '/documentation.html');
+ } else {
+   filePath = 'index.html';
+ }
+
+ fs.readFile(filePath, (err, data) => {
+   if (err) {
+     throw err;
+   }
+
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.write(data);
+   response.end('Hello Node!\n');
+
+});
+
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
-
-// let addr = 'http://localhost:8080/default.html?year=2017&month=february';
-// let q = url.parse(addr, true);
-
-// console.log(q.host); 
-// console.log(q.pathname);
-// console.log(q.search); 
-
-// let qdata = q.query; 
-// console.log(qdata.month); 
