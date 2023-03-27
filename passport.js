2b2485afce6cd4e57712ebed457efcd36b0fee8c
SiderFace <115373215+SiderFace@users.noreply.github.com>
2023-03-27 12:45:11 -0400

    Updating /login code
    This is a workaround to the current mongoose package not supporting callbacks in its node.js drivers.

--- passport.js
+++ passport.js
@@ -11,19 +11,17 @@ passport.use(new LocalStrategy({
    usernameField: 'Username',
    passwordField: 'Password'
 }, (username, password, callback) => {
-      console.log(username + "  " + password);
-      Users.findOne({Username: username}, (error, user) => {
-         if (error) {
-            console.log(error);
-            return callback(error);
-         }
-         if (!user) {
-            console.log("incorrect username");
-            return callback(null, false, {message: "Incorrect username or password."});
-         }
-         console.log('finished');
-         return callback(null, user);
-      });
+   console.log("login attempt", username + "  " + password);
+   Users.findOne({ Username: username }).then((user) => {
+      if (!user) {
+         console.log("incorrect username");
+         return callback(null, false, { message: "Incorrect username or password." });
+      }
+      console.log('finished');
+      return callback(null, user);
+   }).catch((error) => {
+      return callback(error)
+   });
    }
 ));
 
