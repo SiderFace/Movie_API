a60eba947734422338f41b69efe6f16de1798ee1
SiderFace <115373215+SiderFace@users.noreply.github.com>
2023-03-31 02:38:40 -0400

    2.10 - 2nd submission
    - Fixing erroneous config vars on Heroku to seed database with movies.
    
    - debugging app.put function for user info updating.   Adding validators for user info entry fields.

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ const uuid = require('uuid');
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 const { check, validationResult } = require('express-validator');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 //models.js definitions
 const Movies = Models.Movie;
 const Users = Models.User;
+
+
+dotenv.config();
 
 // mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -35,25 +40,22 @@ app.use(morgan('common') );
 app.use(express.static('public') );
 
 
-const cors = require('cors');
+// let allowedOrigins = [
+//    'http://localhost:8080',
+//    'http://testsite.com'
+// ];
+// app.use(cors({
+//   origin: (origin, callback) => {
+//    if(!origin) return callback(null, true);
+//    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//       return callback(new Error(message ), false);
+//    }
+//     return callback(null, true);
+//    }
+// }));
 
-let allowedOrigins = [
-   'http://localhost:8080',
-   'http://testsite.com'
-];
-
-// app.use(cors())   //temp test
-
-app.use(cors({
-  origin: (origin, callback) => {
-   if(!origin) return callback(null, true);
-   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message ), false);
-   }
-    return callback(null, true);
-   }
-}));
+app.use(cors())   //temp test
 
 let auth = require('./auth')(app);
 const passport = require('passport');
@@ -61,7 +63,7 @@ require('./passport');
 
 
 
-//Welcome , ping message
+//Welcome , temp ping
 app.get('/', (req, res) => {
    res.send("! Welcome to my Top 20 Movies !");
 });
@@ -179,11 +181,26 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 });
 
 //UPDATE - Allow users to update their user info
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.put('/users/:Username', [
+   check('Username', 'Username is required').isLength({ min: 5 }),
+   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+   check('Password', 'Password is required').not().isEmpty(),
+   check('Email', 'Email does not appear to be valid').isEmail(),
+],
+passport.authenticate('jwt', { session: false }),
+(req, res) => {
+   let errors = validationResult(req);
+
+   if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+   }
+
+   let hashedPassword = Users.hashPassword(req.body.Password);
+
    Users.findOneAndUpdate({ Username: req.params.Username },
       {$set:{
          Username: req.body.Username,
-         Password: req.body.Password,
+         Password: hashedPassword,
          Email: req.body.Email,
          Birthday: req.body.Birthday
       }},
