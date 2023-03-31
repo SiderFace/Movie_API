db7c5fb3ce694b3b7b2286c07a0601e51f10e711
SiderFace <115373215+SiderFace@users.noreply.github.com>
2023-03-31 01:36:39 -0400

    heroku

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const Users = Models.User;
 
 // mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// mongoose.connect(process.env.CONNECTION_URI, {
-//    useNewUrlParser: true,
-//    useUnifiedTopology: true
-// });
+mongoose.connect(process.env.CONNECTION_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
 
 //Connect to the myFlixDB database
 // mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB');
